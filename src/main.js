babe0f7c830aaf30580e2c68c1210b76565c72f9
jobvank <job@yourfutureacademy.nl>
2022-07-02 15:52:41 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+console.log(process.env.SUPABASE_URL) // remove this after you've confirmed it working
+
 createApp(App).mount('#app')
