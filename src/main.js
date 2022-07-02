6639301b78796f807a3e64636b224cf1d60ad089
jobvank <job@yourfutureacademy.nl>
2022-07-02 17:15:38 +0200

    20220702 vars

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-console.log(process.env.SUPABASE_URL) // remove this after you've confirmed it working
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it working
 
 createApp(App).mount('#app')
